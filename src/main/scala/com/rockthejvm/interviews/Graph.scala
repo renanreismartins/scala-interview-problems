afa68537d72d6f4ab5e358eb53ef4e2e162dc230
Renan Reis Martins de Paula <renanreismartins@gmail.com>
2022-03-07 18:38:56 +0100

    isPath with TR

--- src/main/scala/com/rockthejvm/interviews/Graph.scala
+++ src/main/scala/com/rockthejvm/interviews/Graph.scala
@@ -1,5 +1,7 @@
 package com.rockthejvm.interviews
 
+import scala.annotation.tailrec
+
 object Graph extends App {
   type Graph[T] = Map[T, Set[T]]
 
@@ -9,6 +11,7 @@ object Graph extends App {
     "Charlie" -> Set("David"),
     "David" -> Set("Bob", "Mary"),
     "Mary" -> Set("Bob", "Charlie")
+//    "Mary" -> Set("Bob", "Charlie", "Alice")
   )
 
   def outDegree[T](graph: Graph[T], node: T): Int = graph.getOrElse(node, Set()).size
@@ -30,14 +33,36 @@ object Graph extends App {
     isPathAux(graph.getOrElse(start, Set()), Set())
   }
 
+
+  def isPathTR[T](graph: Graph[T], start: T, end: T): Boolean = {
+    @tailrec
+    def isPathTRAux(toVisit: Set[T], visited: Set[T]): Boolean =
+      if (toVisit.isEmpty) false
+      else if (toVisit.contains(end)) true
+      else {
+        val allNonVisitedChildren = toVisit.flatMap(e => graph.getOrElse(e, Set())) -- visited
+        isPathTRAux(allNonVisitedChildren, visited ++ toVisit)
+      }
+
+    isPathTRAux(graph.getOrElse(start, Set()), Set())
+  }
+
   println("outDegree")
   println(outDegree(socialNetwork, "Alice"))
+
   println()
   println("inDegree")
   println(inDegree(socialNetwork, "David"))
   println()
-  println("inDegree")
+
+  println("isPath")
   println(isPath(socialNetwork, "Alice", "Mary"))
   println(isPath(socialNetwork, "David", "Alice"))
+  println(isPath(socialNetwork, "Bob", "Charlie"))
+
+  println("isPathTR")
+  println(isPathTR(socialNetwork, "Alice", "Mary"))
+  println(isPathTR(socialNetwork, "David", "Alice"))
+  println(isPathTR(socialNetwork, "Bob", "Charlie"))
 
 }
